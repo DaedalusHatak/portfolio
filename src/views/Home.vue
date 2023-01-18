c7050e06fdde754d73ffe825a02bfab01db57f13
DaedalusHatak <pawelkic55@gmail.com>
2023-01-18 10:22:43 +0100

    add button for removing & adding projects to array

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,33 +3,37 @@
 		>Tutaj znajdziesz projekty, przy których pracowałem lub samodzielnie
 		wykonałem</h1
 	>
+
+	<button @click="addProject" class="bg-blue-500">Add</button>
+	<button @click="removeProject(index)" class="bg-red-500">Delete</button>
+
 	<ul
 		:class="gridTemplate"
 		class="m-5 gap-5 grid justify-items-center grid-cols-1 sm:grid-cols-2 place-items-stretch"
 	>
-		<li v-for="project in projects"
+		<li v-for="(project, index) in  projects" 
 			><ProjectCard
 				:name="project.name"
 				:desc="project.description"
 				:tags="project.tags"
 				:image="project.img"
+				:key="index"
 			>
+			
 			</ProjectCard
 		></li>
 	</ul>
 </template>
 
-
-
-
-
-
 <!-- SCRIPT  SCRIPT  SCRIPT  SCRIPT  SCRIPT  SCRIPT  SCRIPT  SCRIPT  SCRIPT  SCRIPT  SCRIPT  SCRIPT  SCRIPT  SCRIPT  SCRIPT  SCRIPT  SCRIPT  SCRIPT  SCRIPT  SCRIPT     -->
-<script setup>
-import { computed } from '@vue/reactivity';
-import ProjectCard from '../components/ProjectCard.vue';
+<script>
 
-const projects = [
+import ProjectCard from '../components/ProjectCard.vue';
+export default{
+	components:{ProjectCard},
+	data(){
+	return{
+	projects: [
 	{
 		name: 'Cięcie wodą 3D',
 		description: 'Zaprojektowana w Vue. Pierwszy większy projekt.',
@@ -42,19 +46,59 @@ const projects = [
 		img: 'vite.svg',
 		tags: ['working', 'title', 'vue'],
 	},
-];
+	{
+		name: 'Working Title 2',
+		description: 'Zaprojektowana w Vue. Pierwszy większy projekt.',
+		img: 'vite.svg',
+		tags: ['working', 'title', 'vue'],
+	},
+	{
+		name: 'Working Title 3',
+		description: 'Zaprojektowana w Vue. Pierwszy większy projekt.',
+		img: 'vite.svg',
+		tags: ['working', 'title', 'vue'],
+	},
+
+	
+],
 
 
-const gridTemplate = computed({
-	get() {
-		console.log(projects.length);
-		if (projects.length === 1) {
+	}
+},
+methods:{
+	addProject() {
+	this.projects.push({
+		name: 'Working Title ',
+		description: 'Zaprojektowana w Vue. Pierwszy większy projekt.',
+		img: 'vite.svg',
+		tags: ['working', 'title', 'vue'],
+	})
+	
+
+},
+removeProject(){
+		this.projects.pop()
+	}
+},
+computed:{
+	
+ gridTemplate(){
+
+		if (this.projects.length === 1) {
 			return 'sm:grid-cols-1 md:grid-cols-1';
-		} else if (projects.length === 2) {
+		} else if (this.projects.length === 2) {
 			return 'md:grid-cols-2';
 		} else {
 			return 'md:grid-cols-3';
 		}
-	},
-});
+
+}
+},
+
+}
+
 </script>
+
+
+
+function 
